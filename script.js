6cbd45856bfca77b28a63e790344e0e1c9a9e354
0xNULLderef <redmastersuper@gmail.com>
2023-11-29 11:43:33 +0100

    fnjdagjndfhjkafghdjk

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
+function V2(x, y) {
+    this.x = x ?? 0;
+    this.y = y ?? 0;
+}
 
+V2.prototype.add = function(other) {
+    return new V2(this.x + other.x, this.y + other.y);
+}
 
-function Ball(x, y, number) {
-    this.x = x;
-    this.y = y;
-    this.rotationX = 0;
-    this.rotationY = 0;
+V2.prototype.sub = function(other) {
+    return new V2(this.x - other.x, this.y - other.y);
+}
+
+V2.prototype.mul = function(scalar) {
+    return new V2(this.x * scalar, this.y * scalar);
+}
+
+V2.prototype.div = function(scalar) {
+    return new V2(this.x / scalar, this.y / scalar);
+}
+
+V2.prototype.dot = function(other) {
+    return this.x * other.x + this.y * other.y;
+}
+
+V2.prototype.length = function() {
+    return Math.sqrt(this.x ** 2 + this.y ** 2);
+}
+
+V2.prototype.normalize = function() {
+    return this.mul(1 / this.length());
+}
+
+function Ball(position, number) {
+    this.position = position;
+    this.rotation = new V2(0, 0);
     this.r = 16;
 
     this.arcToDeg = (Math.PI * (this.r ** 2)) / 360;
 
-    this.velocityX = 0;
-    this.velocityY = 0;
+    this.velocity = new V2(0, 0);
     this.friction = 0.01;
 
     const ball = document.createElement('div');
@@ -32,57 +60,144 @@ function Ball(x, y, number) {
         circleBottomContainer.append(circleBottom);
     }
 
-    const circleFrontContainer = document.createElement('div');
-    ball.append(circleFrontContainer);
+    if(number < 16) {
+        const circleFrontContainer = document.createElement('div');
+        ball.append(circleFrontContainer);
 
-    const circleFront = document.createElement('div');
-    circleFront.classList.add('circle', 'circle-front');
-    circleFrontContainer.append(circleFront);
+        const circleFront = document.createElement('div');
+        circleFront.classList.add('circle', 'circle-front');
+        circleFrontContainer.append(circleFront);
 
-    const circleText = document.createElement('div');
-    circleFront.classList.add('circle-text');
-    circleFront.append(circleText);
+        const circleText = document.createElement('div');
+        circleFront.classList.add('circle-text');
+        circleFront.append(circleText);
 
-    circleText.append(document.createTextNode(number));
+        circleText.append(document.createTextNode(number));
+    }
 
     this.element = ball;
 
-    this.update();
+    if(Ball.balls === undefined) Ball.balls = [];
+    Ball.balls.push(this);
+}
+
+Ball.prototype.move = function(frameTime) {
+    const frameOffset = this.velocity.div(frameTime);
+    this.position = this.position.add(frameOffset);
+    this.rotation = this.rotation.add(frameOffset.mul(this.arcToDeg));
+    this.velocity = this.velocity.mul(1 - this.friction);
+}
+
+Ball.prototype.collide = function(frameTime) {
+    Ball.balls.filter(other => other !== this).forEach((other, i) => {
+        const centerDistance = this.position.sub(other.position).length();
+        if(this.position.sub(other.position).length() < this.r + other.r) {
+            const hitPosition = other.position.sub(this.position);
+            const hitPositionNormalized = hitPosition.normalize();
+            const hitDot = this.velocity.normalize().dot(hitPositionNormalized);
+
+            const hitVelocity = this.velocity.sub(other.velocity).length(); 
+            const velocityChange = hitPositionNormalized.mul(hitVelocity * hitDot);
+            other.velocity = other.velocity.add(velocityChange);
+            this.velocity = this.velocity.sub(velocityChange);
+            this.position = this.position.sub(hitPositionNormalized);
+        }
+    });
 }
 
 Ball.prototype.update = function(frameTime) {
     if(frameTime) {
-        const frameX = this.velocityX / frameTime;
-        const frameY = this.velocityY / frameTime;
-
-        this.x += frameX;
-        this.y += frameY;
-        
-        this.rotationX += frameX * this.arcToDeg;
-        this.rotationY -= frameY * this.arcToDeg;
-
-        this.velocityX *= 1 - this.friction;
-        this.velocityY *= 1 - this.friction;
+        this.move(frameTime);
+        this.collide(frameTime);
     }
-    this.element.style.marginTop = `${this.x}px`;
-    this.element.style.marginLeft = `${this.y}px`;
-    this.element.style.setProperty('--rotationX', `${this.rotationX}deg`);
-    this.element.style.setProperty('--rotationY', `${this.rotationY}deg`);
+
+    this.element.style.marginTop = `${this.position.y}px`;
+    this.element.style.marginLeft = `${this.position.x}px`;
+    this.element.style.setProperty('--rotationX', `${-this.rotation.x}deg`);
+    this.element.style.setProperty('--rotationY', `${this.rotation.y}deg`);
 }
 
-// const thirteen = ;/  
-const eight = new Ball(0, 0, 8);
-document.body.append(eight.element);
-eight.velocityX = 30;
-eight.velocityY = 5;
-// document.body.append(new Ball(0, 0, 8).element);
+const right = new V2(32.5, 0);
+const angle = 60 / 180 * Math.PI;
+const down = new V2(Math.cos(angle) * 32.5, Math.sin(angle) * 32.5);
+const origin = new V2(window.innerWidth / 2 - 32 * 3, 50);
+
+const positions = [
+    origin.add(down.mul(4)), // 1
+    origin.add(down.mul(2).add(right.mul(2))), // 2
+    origin.add(right), // 3
+    origin.add(down), // 4
+    origin.add(down.mul(3)), // 5
+    origin.add(right.mul(4)), // 6
+    origin.add(down).add(right.mul(2)), // 7
+    origin.add(down.mul(2).add(right)), // 8
+    origin.add(down).add(right.mul(3)), // 9
+    origin.add(down.mul(2)), // 10
+    origin.add(down.mul(3).add(right)), // 11
+    origin, // 12
+    origin.add(right.mul(2)), // 13
+    origin.add(down).add(right), // 14
+    origin.add(right.mul(3)), // 15
+    origin.add(right.mul(2)).add(new V2(0.1, 300))
+];
+
+let balls = positions.map((position, index) => new Ball(position, index + 1));
+
+// balls[15].velocity.y = -100;
+// balls[15].velocity.x = -10;
+
+const table = document.getElementById('table');
+balls.forEach(ball => table.append(ball.element));
+
+const divClientRect = table.getBoundingClientRect();
+const divOffset = new V2(divClientRect.top, divClientRect.left);
+
+let relativePosition = new V2(0, -1);
+
+window.addEventListener('mousemove', event => {
+    const clientPosition = new V2(event.clientX, event.clientY);
+    const divPosition = clientPosition.sub(divOffset);
+    relativePosition = balls[15].position.add(new V2(16, 16)).sub(divPosition);
+    stick.style.setProperty('--rotationZ', `${180 - Math.atan2(relativePosition.x, relativePosition.y) / Math.PI * 180}deg`)
+});
+
+let pull = 5;
+window.addEventListener('wheel', event => {
+    if(stick.style.display != 'hidden') {
+        pull += event.deltaY / 20;
+        if(pull > 100) pull = 100;
+        if(pull < 5) pull = 5;
+    }
+});
+
+window.addEventListener('click', () => {
+    if(stick.style.visibility != 'hidden') {
+        balls[15].velocity = relativePosition.normalize().mul((pull ** 2) / 50 + 25);
+        pull = 5;
+        stick.style.visibility = 'hidden';
+        console.log(stick.style.visibility);
+    }
+});
+
+const stick = document.getElementById('stick');
 
 let lastTimeStamp = 0;
 const animate = timeStamp => {
-    // const timeStampNonNaN = timeStamp ?? 16;
     const frameTime = timeStamp - lastTimeStamp;
     lastTimeStamp = timeStamp;
-    eight.update(frameTime);
+    balls.forEach(ball => ball.update(frameTime));
+    
+    if(stick.style.visibility != 'hidden') {
+        let stickPos = balls[15].position.add(new V2(12, 32));
+        stick.style.marginLeft = `${stickPos.x}px`;
+        stick.style.marginTop = `${stickPos.y}px`;
+        stick.style.setProperty('--pull', `${pull}px`);
+    } else {
+        if(balls[15].velocity.length() < 0.5) {
+            balls[15].velocity = new V2(0, 0);
+            stick.style.visibility = '';
+        }
+    }
 
     requestAnimationFrame(animate);
 };
